df191fae3d53725370defd041224155a86553fab
burunyuu23 <ezzfvkoko@gmail.com>
2024-03-13 23:20:12 +0300

    Initial commit

---
+++ src/main/java/ru/dnlkk/ratingusbackend/RatingusBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.dnlkk.ratingusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingusBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dnlkk/ratingusbackend/RatingusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dnlkk.ratingusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingusBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
