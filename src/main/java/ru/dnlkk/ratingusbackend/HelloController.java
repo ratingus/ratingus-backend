bb61bd46e2a8d4341ae3fbcae0de6a69c03ff8fe
burunyuu23 <ezzfvkoko@gmail.com>
2024-03-31 00:19:21 +0300

    НЕУЖЕЛИ

--- src/main/java/ru/dnlkk/ratingusbackend/HelloController.java
+++ src/main/java/ru/dnlkk/ratingusbackend/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @GetMapping
     public ResponseEntity<Dto> get() {
-        return ResponseEntity.ok(new Dto("ping pong"));
+        return ResponseEntity.ok(new Dto("УРААААААА!!!"));
     }
 }
