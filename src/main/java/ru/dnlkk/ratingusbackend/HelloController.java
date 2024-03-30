54f28dfcb11366f5eded00795cfadd2e66acf37f
dnlkk <34377854+burunyuu23@users.noreply.github.com>
2024-03-31 01:07:21 +0300

    RATINGUS-117 Настроил CI/CD (В основном CD) (#6)
    * добавил плейбук ансибла
    
    * добавил гх актионс
    
    * добавил гхцр имейдж в компоуз
    
    * изменил ветку в воркфлоу для теста
    
    * добавил тестовый контроллер
    
    * добавил passphrase
    
    * пытаюсь запустить ансибл
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * что я делаю....
    
    * кажется всё работает =)
    
    * теперь точно всё ЗАработает =)
    
    * финал финалов!!
    
    * финальный финал v2!!!
    
    * финальный финал v3!!!
    
    * НЕУЖЕЛИ
    
    * ПОЖАЛУЙСТА
    
    * ПОЖАЛУЙСТА
    
    * ПОЖАЛУЙСТА
    
    * ПОЖАЛУЙСТА
    
    * ПОЖАЛУЙСТА
    
    * ПОЖАЛУЙСТА
    
    * УРА!!!
    
    * УРА!!!
    
    * поменял ветку на дев (я победил :3)

---
+++ src/main/java/ru/dnlkk/ratingusbackend/HelloController.java
@@ -0,0 +1,14 @@
+package ru.dnlkk.ratingusbackend;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<Dto> get() {
+        return ResponseEntity.ok(new Dto("хелло! хау ар ю? айм файн фенкью!"));
+    }
+}
