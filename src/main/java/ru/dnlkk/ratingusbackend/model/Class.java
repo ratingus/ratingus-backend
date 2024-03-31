1b343f8025911914a398a068f71f0e36eec09dd9
Evgeniy Sakov <91192469+PiPuKaPRo@users.noreply.github.com>
2024-03-31 09:44:55 +0300

    Evg/models (#1)
    * сделал модели
    
    * завершил модели полностью
    
    * добавил инит для базы данных
    
    * исправил таблицу и зависимости класс_объявление
    
    * исправил таблицу

---
+++ src/main/java/ru/dnlkk/ratingusbackend/model/Class.java
@@ -0,0 +1,34 @@
+package ru.dnlkk.ratingusbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "classes")
+public class Class {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "school_id", referencedColumnName = "id")
+    private School school;
+
+    @ManyToMany(mappedBy = "classes")
+    private List<User> students;
+
+    @ManyToMany(mappedBy = "classes")
+    private List<Announcement> announcements;
+
+}
---
+++ src/main/java/ru/dnlkk/ratingusbackend/model/Lesson.java
@@ -0,0 +1,42 @@
+package ru.dnlkk.ratingusbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "lessons")
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "lesson_number")
+    private int lessonNumber;
+
+    @Column(name = "homework")
+    private String homework;
+
+    @Column(name = "theme")
+    private String theme;
+
+    @Column(name = "date_of_lesson")
+    private Timestamp date;
+
+    @ManyToOne
+    @JoinColumn(name = "study_id", referencedColumnName = "id")
+    private Study study;
+
+    @OneToMany(mappedBy = "lesson")
+    private List<StudentLesson> studentLessons;
+}
