0e3e48683dd35e33fe83d488ce758cbb4ab23da6
Павел Сапегин <pavelsapegin18@gmail.com>
2024-05-19 00:02:17 +0300

    Для Дани пуш

---
+++ src/main/java/ru/dnlkk/ratingusbackend/model/TeacherSubject.java
@@ -0,0 +1,17 @@
+package ru.dnlkk.ratingusbackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "student_lesson")
+public class TeacherSubject {
+}
