e44a39099b78bfffcd5ea5d361e82987b50ffdbe
Павел Сапегин <pavelsapegin18@gmail.com>
2024-05-20 21:29:18 +0300

    Работает реализованная ветка объявлений

--- src/main/java/ru/dnlkk/ratingusbackend/api/controllers/ExceptionHandlerController.java
+++ src/main/java/ru/dnlkk/ratingusbackend/api/controllers/ExceptionHandlerController.java
@@ -4,11 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import ru.dnlkk.ratingusbackend.exceptions.ForbiddenException;
+import ru.dnlkk.ratingusbackend.exceptions.LogicException;
 import ru.dnlkk.ratingusbackend.exceptions.NotFoundException;
 
 @RestController
 public class ExceptionHandlerController {
-    @ExceptionHandler({NotFoundException.class, ForbiddenException.class})
+    @ExceptionHandler({NotFoundException.class, ForbiddenException.class, LogicException.class})
     public ResponseEntity<String> handleNotFoundException(RuntimeException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
