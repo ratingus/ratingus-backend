a84030e930c6c9609de74acb28c7ab15ca913692
dnlkk <34377854+burunyuu23@users.noreply.github.com>
2024-06-17 23:32:26 +0300

    фикс (#54)

--- src/main/java/ru/dnlkk/ratingusbackend/repository/AnnouncementRepository.java
+++ src/main/java/ru/dnlkk/ratingusbackend/repository/AnnouncementRepository.java
@@ -14,6 +14,8 @@ public interface AnnouncementRepository extends JpaRepository<Announcement, Inte
     List<Announcement> findByClasses_Id(int classId, PageRequest pageRequest);
     List<Announcement> getAnnouncementsByClassesIn(List<Class> classes, PageRequest pageRequest);
 
+    List<Announcement> findByCreatorSchoolId(int schoolId, PageRequest of);
+
 //    @Query("SELECT a FROM Announcement a WHERE ?1 in a.classes")
 //    List<Announcement> findByClass(String filter);
 }
